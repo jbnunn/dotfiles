c6a6f593921ca4205fb986c5cacc02b15b41bef5
J. Nunn <jbnunn@gmail.com>
2025-12-10 11:54:32 -0600

    Lets me drag ghostty around on Mac. Shows hidden files in nvim.

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -4,9 +4,9 @@ return {
     filesystem = {
       filtered_items = {
         visible = true,
-        show_hidden_count = true,
         hide_dotfiles = false,
         hide_gitignored = false,
+        hide_by_name = {},
         never_show = { ".DS_Store", "thumbs.db" },
       },
     },
